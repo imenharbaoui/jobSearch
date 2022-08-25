e054b96b248c43d94a80a86993c52498ad0793dc
imen harbaoui <imenharbaoui111@gmail.com>
2022-08-25 17:23:49 +0100

    first step on js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+function getinformation(){
+    var cardCin=document.getElementById('TxtCin').value
+    var cardcin2=document.getElementById('TxtCin2').value
+    var TxtNom=document.getElementById('TxtNom').value
+    var TxtPrenom=document.getElementById('TxtPrenom').value
+    var dnaiss=document.getElementById('dnaiss').value
+    var TxtAdresse=document.getElementById('TxtAdresse').value
+    var TxtCp=document.getElementById('TxtCp').value
+    var box1=document.getElementById('box1').value
+    var txttel=document.getElementById('TxtTel').value
+    var TxtMail=document.getElementById('TxtMail').value
+    var login=document.getElementById('login_').value
+    var var_post=document.getElementById('var_post').value
+    
+    
+}
